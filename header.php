2ff81a31a51f896f5eaa8c5bfa58a4665d133425
y.bach <y.bach@intronic.ch>
2018-04-18 10:06:40 +0200

    edit
    Einfügen des Favicons, einbetten des eigenen Stylesheets und erstes Grundgerüst mit Bootstrap.

--- header.php
+++ header.php
@@ -12,7 +12,9 @@
 		<meta charset="<?php bloginfo( 'charset' ); ?>" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 		<?php // wp_head(); ?>
+		<link rel="icon" href="<?php bloginfo ('template_directory') ?>/favicon.ico">
 		<link rel="stylesheet" href="<?php bloginfo('template_directory')?>/css/bootstrap.min.css"> <!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="<?php bloginfo('template_directory')?>/style.css"> <!-- Eigenes CSS -->
 	</head>
 	<body <?php body_class(); ?>>
 
@@ -20,8 +22,20 @@
 		<?php get_template_part( 'template-parts/mobile-off-canvas' ); ?>
 	<?php endif; ?>
 
+<header>
+<div class="container">
+	<div class="row main">
+		<div class="col-lg">
+			<img src="<?php bloginfo('template_directory')?>/img/introniclogo.JPG" width="200px">
+		</div>
+	</div>
+	<div class="row main">
 
-	<header class="site-header" role="banner">
+	</div>
+</div>
+</header>
+
+<!-- 	<header class="site-header" role="banner">
 		<div class="site-title-bar title-bar" <?php foundationpress_title_bar_responsive_toggle(); ?>>
 			<div class="title-bar-left">
 				<button aria-label="<?php _e( 'Main Menu', 'foundationpress' ); ?>" class="menu-icon" type="button" data-toggle="<?php foundationpress_mobile_menu_id(); ?>"></button>
@@ -46,4 +60,4 @@
 			</div>
 		</nav>
 
-	</header>
+	</header> -->
