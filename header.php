9ad7159c37343edf3e65d4b87a01b2a9b2201bfa
y.bach <y.bach@intronic.ch>
2018-04-18 10:50:24 +0200

    Upload Favicon

--- header.php
+++ header.php
@@ -12,8 +12,7 @@
 		<meta charset="<?php bloginfo( 'charset' ); ?>" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 		<?php // wp_head(); ?>
-		<link rel="icon" href="<?php bloginfo ('template_directory') ?>/favicon.ico">
-		<link rel="stylesheet" href="<?php bloginfo('template_directory')?>/css/bootstrap.min.css"> <!-- Bootstrap CSS -->
+		<link rel="icon" href="<?php bloginfo ('template_directory') ?>/favicon.ico"> <!-- Einbettung des Favicon -->
 		<link rel="stylesheet" href="<?php bloginfo('template_directory')?>/style.css"> <!-- Eigenes CSS -->
 	</head>
 	<body <?php body_class(); ?>>
@@ -22,42 +21,21 @@
 		<?php get_template_part( 'template-parts/mobile-off-canvas' ); ?>
 	<?php endif; ?>
 
-<header>
+<header class="starter">
 <div class="container">
 	<div class="row main">
 		<div class="col-lg">
-			<?php if ( function_exists( 'the_custom_logo' ) ) { the_custom_logo();} ?>
+			<?php if ( function_exists( 'the_custom_logo' ) ) { the_custom_logo();} ?> <!-- Funktion für ein eigens bestimmtes Logo -->
 		</div>
 	</div>
 	<div class="row main">
-
-	</div>
-</div>
-</header>
-
-<!-- 	<header class="site-header" role="banner">
-		<div class="site-title-bar title-bar" <?php foundationpress_title_bar_responsive_toggle(); ?>>
-			<div class="title-bar-left">
-				<button aria-label="<?php _e( 'Main Menu', 'foundationpress' ); ?>" class="menu-icon" type="button" data-toggle="<?php foundationpress_mobile_menu_id(); ?>"></button>
-				<span class="site-mobile-title title-bar-title">
-					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
-				</span>
-			</div>
+		<div class="col-lg">
+			<nav class="navbar navbar-expand-md navbar-dark bg-dark" role="navigation">
+				<ul class="nav navbar-nav">
+			<?php foundationpress_top_bar_r(); ?> <!-- Menüstruktur von Wordpress -->
+				</ul>
+			</nav>
 		</div>
-
-		<nav class="site-navigation top-bar" role="navigation">
-			<div class="top-bar-left">
-				<div class="site-desktop-title top-bar-title">
-					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
-				</div>
-			</div>
-			<div class="top-bar-right">
-				<?php foundationpress_top_bar_r(); ?>
-
-				<?php if ( ! get_theme_mod( 'wpt_mobile_menu_layout' ) || get_theme_mod( 'wpt_mobile_menu_layout' ) === 'topbar' ) : ?>
-					<?php get_template_part( 'template-parts/mobile-top-bar' ); ?>
-				<?php endif; ?>
-			</div>
-		</nav>
-
-	</header> -->
+	</div>
+</div> <!-- Container -->
+</header>
