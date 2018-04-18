88b4ef6e8f8d66fab2758578d5d5470cf2e2f694
y.bach <y.bach@intronic.ch>
2018-04-18 09:24:54 +0200

    Bootstrap CSS
    Stylesheet von Bootstrap integrieren

--- header.php
+++ header.php
@@ -11,7 +11,8 @@
 	<head>
 		<meta charset="<?php bloginfo( 'charset' ); ?>" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-		<?php wp_head(); ?>
+		<?php // wp_head(); ?>
+		<link rel="stylesheet" href="<?php bloginfo('template_directory')?>/css/bootstrap.min.css"> <!-- Bootstrap CSS -->
 	</head>
 	<body <?php body_class(); ?>>
 
