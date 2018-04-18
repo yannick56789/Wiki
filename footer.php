2e6a30f125825ea648abb85e6a7111dd0263e3f7
y.bach <y.bach@intronic.ch>
2018-04-18 09:29:57 +0200

    Bootsrap Javascript
    Integration des Javascripts für Bootstrap anwendungen

--- footer.php
+++ footer.php
@@ -20,6 +20,7 @@
 <?php if ( get_theme_mod( 'wpt_mobile_menu_layout' ) === 'offcanvas' ) : ?>
 	</div><!-- Close off-canvas content -->
 <?php endif; ?>
+<script rel="javascript" src="<?php bloginfo('template_directory')?>/js/bootstrap.min.js"> <!-- Bootstrap Javascript -->
 
 <?php wp_footer(); ?>
 </body>
