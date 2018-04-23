1eb86d49800aa0614d7307410b7abd116c2114a3
y.bach <y.bach@intronic.ch>
2018-04-23 11:34:08 +0200

    edit

--- 404.php
+++ 404.php
@@ -9,40 +9,19 @@ Author:             Yannick Bach
 get_header(); ?>
 
 <div class="container">
-	<div class="row main">
-		<div class="col-md-8">
-			<article>
+	<div class="row main notfound">
+		<div class="col-lg">
+			<div class="entry-content">
 				<div class="fiernullfier">
-					<i class="fas fa-exclamation-triangle fa-9x"></i>
-					<h1 class="entry-title"><p>Seite konnte leider nicht gefunden werden.</p></h1>
-				</div>
-				<div class="entry-content">
 					<div class="error">
-						<p class="bottom">Die Seite, die Sie suchen, wurde möglicherweise entfernt oder umbennant, oder ist vorübergehend nicht verfügbar. Vielleicht hilft eine Suche</p>
+						<h1 class="errortitel">404</h1>
+						<p class="bottom">Die Seite, die Sie suchen, wurde möglicherweise entfernt oder umbennant, oder ist vorübergehend nicht verfügbar.</p>
 					</div>
 					<p><?php get_search_form(); ?></p>
-					<ul>
-						<li>
-							<?php _e( 'Überprüfen Sie ihre Eingabe auf Rechtschreibefehler', 'foundationpress' ); ?>
-						</li>
-						<li>
-							<?php
-								printf(
-									__( 'Zurück zur <a href="%s">Startseite</a>', 'foundationpress' ),
-									home_url()
-								);
-							?>
-						</li>
-						<li>
-							<?php _e( 'Eine Seite <a href="javascript:history.back()">zurück</a>', 'foundationpress' ); ?>
-						</li>
-					</ul>
 				</div>
-			</article>
-		</div>
-		<div class="col-md-4">
-			<?php get_sidebar(); ?>
+			</div>
 		</div>
 	</div>
 </div>
+
 <?php get_footer();
