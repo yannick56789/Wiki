ddf9b236b187a8936d3699d2fc3c8a846038c1a8
y.bach <y.bach@intronic.ch>
2018-04-26 16:13:35 +0200

    Seiteninhalt nur für Benutzer zugänglich gemacht

--- page.php
+++ page.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Theme Name:         Wiki
-Beschreibung:       Das standart für Beiträge
+Beschreibung:       Das standart File für Seiten
 Version:            0.5
 Author:             Yannick Bach
 */
@@ -11,15 +11,27 @@ get_header(); ?>
 <div class="container">
 	<div class="row main">
 		<div class="col-md-8">
-			<?php while ( have_posts() ) : the_post(); // Beginn Post Loop // ?>
-				<?php get_template_part( 'template-parts/content', 'page' ); ?>
-				<?php comments_template(); ?>
-			<?php endwhile; // Ende Post Loop // ?>
+			<div class="crumb">
+				<?php if (function_exists('nav_breadcrumb')) nav_breadcrumb(); ?> <!-- Breadcrumb Navigation Funktion -->
+			</div>
+			<article>
+				<?php
+				if ( !is_user_logged_in() ){ // Macht eine Abfrage ob der Nutzer angemeldet ist //
+					the_title( '<h1 class="titel">', '</h1>' ); // Zeigt den Titel der Seite //
+					echo "Sie müssen eingeloggt sein um diesen Inhalt zu sehen."; }
+				else {
+					the_title( '<h1 class="titel">', '</h1>' ); // Zeigt den Titel der Seite //
+				while ( have_posts() ) : the_post(); // Ist der Nutzer angemeldet wird der Inhalt ausgegeben //
+					the_content('');
+				endwhile;
+				}
+				?>
+			</article>
 		</div>
 		<div class="col-md-4">
 			<?php get_sidebar(); ?> <!-- WordPress Seitennavigation -->
 		</div>
 	</div> <!-- Row -->
 </div> <!-- Container -->
-<?php
-get_footer();
+
+<?php get_footer();
